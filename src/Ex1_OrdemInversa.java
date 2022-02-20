f63163e578ea695048ecc97b4677791e7e240ad5
gsdr3112 <gsdr3112@hotmail.com>
2022-02-20 16:10:27 -0300

    Ex1 array ordem inversa

---
+++ src/Ex1_OrdemInversa.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Ex1_OrdemInversa {
+    public static void main(String[] args) {
+        array1();
+    }
+
+    public static void array1() {
+        int[] array = {1, 2, 3, 4, 5, 6};
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.println(array[i]);
+
+        }
+
+    }
+
+
+}
